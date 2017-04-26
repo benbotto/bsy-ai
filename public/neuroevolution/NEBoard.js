88c99b9d0e06d7a56825c9fa66c3745e8bb26ab9
Ben Botto <benjamin.botto@gmail.com>
2017-04-25 17:20:17 -0700

    Player and Fruit now stored in board, with accessor methods.

--- public/neuroevolution/NEBoard.js
+++ public/neuroevolution/NEBoard.js
@@ -6,10 +6,11 @@
       'WorldObject',
       'vec2',
       'NEPlayer',
+      'NEFruit',
       NEBoardProducer
     ]);
 
-  function NEBoardProducer(WorldObject, vec2, NEPlayer) {
+  function NEBoardProducer(WorldObject, vec2, NEPlayer, NEFruit) {
     const FRUIT  = 1;
     const PLAYER = 2;
 
@@ -21,41 +22,45 @@
       constructor(width, height) {
         super('NEBoard');
 
-        this.width       = width;
-        this.height      = height;
-        this.player      = new NEPlayer(width, height);
-        this._boardState = [];
+        this.width  = width;
+        this.height = height;
+        this.player = new NEPlayer(width, height);
+        this.fruit  = null;
 
-        // Initialize the board to all 0s (no occupants).
-        for (let i = 0; i < height; ++i)
-          this._boardState.push(Array.from({length: this.width}, () => 0));
-
-        // Player's location.
-        this._boardState[this.player.getX()][this.player.getY()] = PLAYER;
+        this.moveFruit();
       }
 
       /**
-       * Get a random unoccupied location.
+       * Move the fruit to a random position such that it's not on top
+       * of the player.
        */
-      _getRandomLocation() {
-        let point    = null;
-
-        while (!point) {
-          const x = Math.floor(Math.random() * this.width);
-          const y = Math.floor(Math.random() * this.height);
-
-          if (this._boardState[x][y] === 0)
-            point = vec2.fromValues(x, y);
+      moveFruit() {
+        do {
+          this.fruit = new NEFruit(
+            Math.floor(Math.random() * this.width),
+            Math.floor(Math.random() * this.height));
         }
-
-        return point;
+        while (this.playerHitsFruit());
       }
 
       /**
        * Get the piece at location x, y.
        */
       getPieceAt(x, y) {
-        return this._boardState[x][y];
+        if (this.player.getX() === x && this.player.getY() === y)
+          return this.player;
+        else if (this.fruit.getX() === x && this.fruit.getY() === y)
+          return this.fruit;
+        else
+          return null;
+      }
+
+      /**
+       * Check if the player hits the fruit.
+       */
+      playerHitsFruit() {
+        return this.player.getX() === this.fruit.getX() &&
+               this.player.getY() === this.fruit.getY();
       }
     }
 
